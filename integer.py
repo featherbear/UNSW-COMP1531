69fe3665a28fc662e0b0be6b816baec4184d0eb9
Andrew Wong <andrew.j.wong@outlook.com>
2019-02-12 14:14:05 +1100

    Complete exercise 2

--- integer.py
+++ integer.py
@@ -1,2 +1,11 @@
 integers = [1, 2, 3, 4, 5]
 total = 0
+
+integers.append(6)
+
+for value in integers:
+  total += value
+
+print("Sum via for loop: %d" % total)
+print("Sum via sum func: %d" % sum(integers))
+
