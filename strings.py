42796d591a24783c7fd69ebd1e0dbf92f54d08f1
Andrew Wong <andrew.j.wong@outlook.com>
2019-02-12 14:19:28 +1100

    Complete exercise 3

--- strings.py
+++ strings.py
@@ -1,2 +1,12 @@
 strings = ['This', 'list', 'is', 'now', 'all', 'together']
 sentence = ''
+
+for word in strings:
+  sentence += word + " "
+
+# Now trim the trailing space
+# or sentence = sentence.strip()
+sentence = sentence[:-1]
+
+print("Concatenation via for loop: %s" % sentence)
+print("Concatenation via str.join: %s" % " ".join(strings))
